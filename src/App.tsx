3e507814fb5243bcfa4dba5cae7413078ed1be4e
havardnyboe <40893697+havardnyboe@users.noreply.github.com>
2024-09-03 18:12:47 +0200

    enkel prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
+import { HGrid } from "@navikt/ds-react";
 import "./App.css";
 import Kobling from "./Kobling";
 
 function App() {
   return (
-    <>
-      <main>
-        <h1>Umiddelbart</h1>
-        <Kobling />
-      </main>
-    </>
+    <main>
+      <h1>Umiddelbart</h1>
+      <HGrid
+        columns={{ xs: 2, sm: 2, md: 4, lg: 5, xl: 6 }}
+        gap="2"
+        align="center"
+      >
+        <Kobling url="https://nav.no" />
+        <Kobling url="https://skatteetaten.no" />
+        <Kobling url="https://dagenidag.com" />
+        <Kobling url="https://cloudflare.com" />
+        <Kobling url="https://www.dagsdato.no" />
+        <Kobling url="https://nav.no" />
+        <Kobling url="https://nav.no" />
+        <Kobling url="a" />
+      </HGrid>
+    </main>
   );
 }
 
--- src/Kobling.tsx
+++ src/Kobling.tsx
@@ -1,12 +1,47 @@
+import { Box, VStack } from "@navikt/ds-react";
 import "react";
 
-function Kobling() {
+interface KoblingProps {
+  url: string;
+}
+
+function Kobling({ url }: KoblingProps) {
+  const bareUrl = url.replace(/(^\w+:|^)\/\//, "");
+  const domene =
+    /(?:[-a-zA-Z0-9@:%_+~.#=]{2,256}\.)?([-a-zA-Z0-9@:%_+~#=]*)\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_+.~#?&//=]*)/g.exec(
+      url
+    ) || "";
+
   return (
-    <div>
-      <img src="" alt="" />
-      <a href="http://"></a>
-      <span></span>
-    </div>
+    <Box
+      as={"div"}
+      className="kobling"
+      margin={"2"}
+      shadow="small"
+      borderColor="border-default"
+      borderRadius={"large"}
+    >
+      <VStack
+        height={"168px"}
+        width={"168px"}
+        href={url}
+        as={"a"}
+        align={"center"}
+        justify={"space-evenly"}
+        padding={"2"}
+      >
+        <img
+          src={
+            !(url == "")
+              ? `https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=http://${bareUrl}&size=128`
+              : "https://upload.wikimedia.org/wikipedia/commons/8/8d/Globe_smoke_rounded.svg"
+          }
+          alt={url}
+          width={"100px"}
+        />
+        <span>{domene[1]}</span>
+      </VStack>
+    </Box>
   );
 }
 
