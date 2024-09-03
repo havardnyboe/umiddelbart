9aacc0199f0438b5a479d97621ef86c137991044
havardnyboe <40893697+havardnyboe@users.noreply.github.com>
2024-09-03 10:58:13 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Kobling from "./Kobling";
+
+function App() {
+  return (
+    <>
+      <main>
+        <h1>Umiddelbart</h1>
+        <Kobling />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Kobling.tsx
@@ -0,0 +1,13 @@
+import "react";
+
+function Kobling() {
+  return (
+    <div>
+      <img src="" alt="" />
+      <a href="http://"></a>
+      <span></span>
+    </div>
+  );
+}
+
+export default Kobling;
